36c0aa83b4c4fcf84184c6759769c0af40757ab0
Vishnu161515101 <bumannagarivishnu143@gmail.com>
2024-04-22 11:16:38 +0530

    second

--- project_folder/app_folder/models.py
+++ project_folder/app_folder/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class dajngo_project(models.Model):
+    name_for_user=models.CharField(max_length=500)
+    email_for_user=models.CharField(max_length=500)
+    password_for_user=models.CharField(max_length=500)
--- project_folder/project_folder/settings.py
+++ project_folder/project_folder/settings.py
@@ -75,8 +75,11 @@ WSGI_APPLICATION = 'project_folder.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vishnu',
+        'USER':'vishnu_vardhan',
+        'PASSWORD':'vishnupooja',
+        'HOST':'localhost'
     }
 }
 
