c8fc2bb7c69c39dd7d4346408802aaaa1950693a
Vishnu161515101 <bumannagarivishnu143@gmail.com>
2024-04-22 11:43:08 +0530

    fith

--- project_folder/app_folder/views.py
+++ project_folder/app_folder/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.views.decorators.csrf import csrf_protect
+@csrf_protect
 
 # Create your views here.
 
@@ -14,7 +15,13 @@ def vardhan(request):
 
 
 def insert_data(request):
-    name_for_user=request.POST['user_name']
-    email_for_user=request.POST['Emails']
-    password_for_user=request.POST['Password_use']
-    return HttpResponse(name_for_user+email_for_user+password_for_user)
+    if request.method == 'POST':
+        # Check if the request method is POST before accessing POST data
+        name_for_user = request.POST.get('user_name', '')  # Use .get() to avoid KeyError
+        email_for_user = request.POST.get('Emails', '')    # Use .get() to avoid KeyError
+        password_for_user = request.POST.get('Password_use', '')  # Use .get() to avoid KeyError
+        # Do something with the form data, such as saving it to a database
+        return HttpResponse(name_for_user + email_for_user + password_for_user)
+    else:
+        # Handle other HTTP methods (GET, etc.) if needed
+        return HttpResponse("This view expects POST requests only.")
