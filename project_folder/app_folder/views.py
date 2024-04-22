545c47895a7b5dbf77d840d440ac2ecdadc78dee
Vishnu161515101 <bumannagarivishnu143@gmail.com>
2024-04-22 13:11:33 +0530

    seventh

--- project_folder/app_folder/views.py
+++ project_folder/app_folder/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
+from app_folder.models import dajngo_project
 from django.views.decorators.csrf import csrf_protect
 @csrf_protect
+
+
 
 # Create your views here.
 
@@ -15,7 +18,9 @@ def vardhan(request):
 
 
 def insert_data(request):
-    name_for_user=request.POST['user_name']
-    email_for_user=request.POST['Emails']
-    password_for_user=request.POST['Password_use']
-    return HttpResponse(name_for_user+email_for_user+password_for_user)
+    name_for_user1=request.POST['user_name']
+    email_for_user1=request.POST['Emails']
+    password_for_user1=request.POST['Password_use']
+    c1=dajngo_project.objects.create(name_for_user=name_for_user1,email_for_user=email_for_user1,password_for_user=password_for_user1)
+    c1.save()
+    return HttpResponse('its done')
